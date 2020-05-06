f62261ecc2468ae26a99beca86cee1f7cb78a299
Tai Van <vcmtspkt@gmail.com>
2020-05-06 10:51:45 +0700

    - Create prototype SuperMushroom to control SuperMushroom behavior.

---
+++ Assets/Scripts/PowerUp/SuperMushroom.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SuperMushroom : Mushroom
+{
+    protected override void Awake()
+    {
+        base.Awake();
+        Type = PowerUpType.SUPER;
+    }
+
+    protected override void Patrol()
+    {
+        throw new System.NotImplementedException();
+    }
+}
