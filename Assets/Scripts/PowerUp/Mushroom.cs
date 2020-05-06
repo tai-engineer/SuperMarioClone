358f3c685f6b9858e0df81df9e370a9c6c1524fa
Tai Van <vcmtspkt@gmail.com>
2020-05-06 10:45:25 +0700

    - Add abstract class Mushroom.

---
+++ Assets/Scripts/PowerUp/Mushroom.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(CircleCollider2D))]
+[RequireComponent(typeof(AudioSource))]
+[RequireComponent(typeof(Rigidbody2D))]
+public abstract class Mushroom: MonoBehaviour
+{
+    public enum PowerUpType
+    {
+        SUPER
+    };
+
+    public PowerUpType Type {get; protected set;}
+
+    #region Components
+    protected Rigidbody2D _rb;
+    protected AudioSource _audio;
+    protected SpriteRenderer _sprite;
+    #endregion
+
+    #region Events
+    public event EventsManager.MushroomCollisionEvent MushroomCollision;
+    #endregion
+    protected virtual void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        _audio = GetComponent<AudioSource>();
+        _sprite = GetComponent<SpriteRenderer>();
+    }
+    protected abstract void Patrol();
+    protected virtual void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Mario"))
+        {
+            MushroomCollision(Type);
+            Destroy(gameObject);
+        }
+    }
+}
