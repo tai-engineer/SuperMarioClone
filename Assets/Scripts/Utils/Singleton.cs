4ea9191abb55cc396719c0e8ef5b9f15cca768a5
Tai Van <vcmtspkt@gmail.com>
2020-05-06 13:30:45 +0700

    - Add template class Singleton.

---
+++ Assets/Scripts/Utils/Singleton.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Singleton<T>: MonoBehaviour where T : Singleton<T>
+{
+    static T _instance;
+
+    public static T Instance
+    {
+        get { return _instance; }
+    }
+
+    protected virtual void Awake()
+    {
+        if (_instance == null)
+            _instance = (T) this;
+        else
+        {
+            Debug.LogError("[SingleTon] Trying to instatiate a second singleton class");
+        }
+    }
+
+    protected virtual void OnDestroy()
+    {
+        if(_instance != null)
+        {
+            _instance = null;
+        }
+    }
+}
