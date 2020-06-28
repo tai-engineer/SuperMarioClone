63d37004bea8a9e705752a135fe464aa8537c6f7
Tai Van <vcmtspkt@gmail.com>
2020-06-28 13:07:58 +0700

    - Create Coin prefab with animation and sound asset.

---
+++ Assets/Scripts/Coin/Coin.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(AudioSource))]
+public class Coin : MonoBehaviour
+{
+    public float bounceDistance;
+    public float gravity;
+    public AudioEvent coinSound;
+
+    Rigidbody2D _rb;
+    AudioSource _audioPlayer;
+    void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        _audioPlayer = GetComponent<AudioSource>();
+    }
+    void Start()
+    {
+        coinSound.Play(_audioPlayer);
+        _rb.gravityScale = gravity;
+        _rb.AddForce(new Vector2(0f, _rb.position.y + bounceDistance), ForceMode2D.Impulse);
+    }
+}
