d8ed2bed73d00d9fdcb63db083b1f6f1b2be2144
Tai Van <vcmtspkt@gmail.com>
2020-07-17 21:39:28 +0700

    - Create BrickBehaviour.cs. - Add Shatter, Bounce, SpawnObject features into BlockBehaviour.cs. - Create Block/Editor/BlockBehaviourEditor.

---
+++ Assets/Scripts/Block/BrickBehaviour.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class BrickBehaviour : MonoBehaviour
+{
+    Vector3 _shatterVector;
+    float _shatterForce = 1.0f;
+    bool _startShatter = false;
+    Rigidbody2D _rb;
+    void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        if(_startShatter)
+        {
+            Shatter();
+            StopShatter();
+        }
+    }
+
+    /* Get destroy when moving out of camera view */
+    void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+    public void SetForce(Vector3 force)
+    {
+        _shatterVector = force;
+    }
+
+    public void StartShatter()
+    {
+        _startShatter = true;
+    }
+    public void StopShatter()
+    {
+        _startShatter = false;
+    }
+    void Shatter()
+    {
+        _rb.AddForce(_shatterVector * _shatterForce, ForceMode2D.Impulse);
+    }
+}
---
+++ Assets/Scripts/Block/Editor/BlockBehaviourEditor.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(BlockBehaviour))]
+[CanEditMultipleObjects]
+public class BlockBehaviourEditor : Editor
+{
+    BlockBehaviour block;
+    void OnEnable()
+    {
+        block = (BlockBehaviour)target;
+    }
+    public override void OnInspectorGUI()
+    {
+        // PowerUp
+        block.spawnType = (BlockBehaviour.SpawnObjectType)EditorGUILayout.EnumPopup("Spawn Type", block.spawnType);
+
+        switch (block.spawnType)
+        {
+            case BlockBehaviour.SpawnObjectType.SuperMushroom:
+                block.SuperMushroom = (GameObject)EditorGUILayout.ObjectField("SuperMushroom Prefab", block.SuperMushroom, typeof(GameObject), false);
+                break;
+            case BlockBehaviour.SpawnObjectType.FireFlower:
+                block.FireFlower = (GameObject)EditorGUILayout.ObjectField("FireFlower Prefab", block.FireFlower, typeof(GameObject), false);
+                break;
+            case BlockBehaviour.SpawnObjectType.Starman:
+                block.Starman = (GameObject)EditorGUILayout.ObjectField("Starman Prefab", block.Starman, typeof(GameObject), false);
+                break;
+            case BlockBehaviour.SpawnObjectType.OneUpMushroom:
+                block.OneUpMushroom = (GameObject)EditorGUILayout.ObjectField("OneUpMushroom Prefab", block.OneUpMushroom, typeof(GameObject), false);
+                break;
+            case BlockBehaviour.SpawnObjectType.Coin:
+                block.Coin = (GameObject)EditorGUILayout.ObjectField("Coin Prefab", block.Coin, typeof(GameObject), false);
+                block.numberOfCoins = EditorGUILayout.IntField("Number of Coins", block.numberOfCoins);
+                block.coinAppearTime = EditorGUILayout.FloatField("Appear time", block.coinAppearTime);
+                break;
+            case BlockBehaviour.SpawnObjectType.None:
+                EditorGUILayout.ObjectField("Brick Prefab", block.brick, typeof(GameObject), false);
+                block.brickShatterSound = (AudioEvent)EditorGUILayout.ObjectField("Shatter Sound", block.brickShatterSound, typeof(AudioEvent), false);
+                break;
+        }
+        block.deactivateSprite = (Sprite)EditorGUILayout.ObjectField("Deactivate Sprite", block.deactivateSprite, typeof(Sprite), false);
+        // Test
+        EditorGUILayout.LabelField("Test", EditorStyles.boldLabel);
+        block.characterType = (BlockBehaviour.CharacterType)EditorGUILayout.EnumPopup("Character Type", block.characterType);
+    }
+}
