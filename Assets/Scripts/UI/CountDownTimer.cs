639b1d653c3efd97ded3e5cd75470e6c736c7a74
Tai Van <vcmtspkt@gmail.com>
2020-10-03 10:39:20 +0700

    - Upgrade project version to 2019.4.1f1 - Add TextMeshPro extra resources - Add in-game UI - Remove unit test framework folder

---
+++ Assets/Scripts/UI/CountDownTimer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class CountDownTimer : MonoBehaviour
+{
+    TextMeshProUGUI _counter;
+    float _timer = 0f;
+    int timeLimit = 400;
+    void Awake()
+    {
+        _counter = GetComponent<TextMeshProUGUI>();
+    }
+    
+    void Update()
+    {
+        _timer += Time.deltaTime;
+        int seconds = (int)_timer % 60;
+        _counter.SetText("" + (timeLimit - seconds));
+    }
+}
