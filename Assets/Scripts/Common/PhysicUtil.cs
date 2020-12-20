d147790889f71bc743937ee6aa145ba1da2388f4
Tai Van <vcmtspkt@gmail.com>
2020-12-20 15:33:16 +0700

    - Update BasicEnemyController - Create PhysicUtil.cs - Make static method ObstacleCheck and GroundCheck - Replace physic checks with new ones

---
+++ Assets/Scripts/Common/PhysicUtil.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicUtil
+{
+    // offset to avoid line cast detect parent object's collider
+    public static bool GroundCheck(Vector2 position, float width, float distance, LayerMask layer, float offset = 0f, bool debugDraw = false)
+    {
+        RaycastHit2D leftHit, centerHit, rightHit;
+        Vector2 leftPos, centerPos, rightPos;
+        Vector2 leftEndPos, centerEndPos, rightEndPos;
+
+        leftPos   = new Vector2(position.x - width, position.y - offset);
+        centerPos = new Vector2(position.x, position.y - offset);
+        rightPos  = new Vector2(position.x + width, position.y - offset);
+
+        leftEndPos = new Vector2(leftPos.x, leftPos.y - distance);
+        centerEndPos = new Vector2(centerPos.x, centerPos.y - distance);
+        rightEndPos = new Vector2(rightPos.x, rightPos.y - distance);
+
+        leftHit = Physics2D.Linecast(leftPos, leftEndPos, layer);
+        centerHit = Physics2D.Linecast(centerPos, centerEndPos, layer);
+        rightHit = Physics2D.Linecast(rightPos, rightEndPos, layer);
+
+        if(debugDraw)
+        {
+            Debug.DrawLine(leftPos, leftEndPos, Color.red);
+            Debug.DrawLine(centerPos, centerEndPos, Color.red);
+            Debug.DrawLine(rightPos, rightEndPos, Color.red);
+        }
+
+        return (leftHit.collider != null || centerHit.collider != null ||
+            rightHit.collider != null);
+    }
+    public static bool ObstacleCheck(Vector2 position, bool left, float distance, LayerMask layer, float offset = 0f, bool debugDraw = false)
+    {
+        RaycastHit2D hit;
+        Vector2 pos, endPos;
+
+        pos = position;
+        pos.x = left ? position.x - offset : position.x + offset;
+
+        endPos = pos;
+        endPos.x = left ? pos.x - distance : pos.x + distance;
+
+        hit = Physics2D.Linecast(pos, endPos, layer);
+
+        if(debugDraw)
+        {
+            Debug.DrawLine(pos, endPos, Color.red);
+        }
+
+        return hit.collider != null;
+    }
+}
--- Assets/Scripts/Enemies/BasicEnemyController.cs
+++ Assets/Scripts/Enemies/BasicEnemyController.cs
@@ -5,33 +5,42 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 [RequireComponent(typeof(AudioSource))]
 [RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(BoxCollider2D))]
 public class BasicEnemyController : MonoBehaviour, IDamageable, IScore
 {
     #region References
     Rigidbody2D _rb;
     AudioSource _audioPlayer;
     Animator _anim;
+    BoxCollider2D _box;
     #endregion
 
-    #region Private Variables
-    [SerializeField]
-    Transform wallCheckPoint;
-    [SerializeField]
-    float wallCheckDistance = 0.2f;
-    [SerializeField]
-    LayerMask layer;
+    #region Audio
     [SerializeField]
     AudioClip _deadClip;
-    [SerializeField]
-    GameObject _scorePopUp;
+    #endregion
+
+    #region Enemy Definition
     [SerializeField]
     BasicEnemy_SO _enemyDefinition;
+    #endregion
 
-    int _faceDirection = 1;
+    #region Physics
+    float _distanceToGnd = 0.1f;
+    float _distanceBtwPoints = 0.45f;
+    float _distanceToObs = 0.1f;
+
     bool _isWallDetected = false;
     bool _isDead = false;
+    bool _isGrounded = false;
+
+    [SerializeField]
+    LayerMask _layerToCheck;
+    int _faceDirection = 1;
     Vector2 _moveVector;
     #endregion
+    [SerializeField]
+    bool _debugDraw = false;
 
     public BasicEnemy_SO EnemyDefinition { get { return _enemyDefinition; } }
 
@@ -40,8 +49,8 @@ public class BasicEnemyController : MonoBehaviour, IDamageable, IScore
         _rb = GetComponent<Rigidbody2D>();
         _audioPlayer = GetComponent<AudioSource>();
         _anim = GetComponent<Animator>();
+        _box = GetComponent<BoxCollider2D>();
     }
-
     void FixedUpdate()
     {
         if(_isDead)
@@ -50,18 +59,28 @@ public class BasicEnemyController : MonoBehaviour, IDamageable, IScore
         }
 
         WallCheck();
-        if (_isWallDetected)
-        {
-            Flip();
-        }
+        GroundCheck();
         Patrol();
     }
     void Patrol()
     {
-         _moveVector = new Vector3(_enemyDefinition.speed * _faceDirection * Time.fixedDeltaTime, 0f);
+        if (_isWallDetected)
+        {
+            Flip();
+        }
+
+        _moveVector = new Vector2(_enemyDefinition.speed * _faceDirection * Time.fixedDeltaTime, 0f);
+
+        if(!_isGrounded)
+        {
+            Stop();
+        }
         _rb.MovePosition(_rb.position + _moveVector);
     }
-
+    void Stop()
+    {
+        _moveVector = Vector2.zero;
+    }
     public void TakeDamage(int damgage)
     {
         if (_isDead)
@@ -72,48 +91,46 @@ public class BasicEnemyController : MonoBehaviour, IDamageable, IScore
         _enemyDefinition.health = _enemyDefinition.health < 0 ? 0 : _enemyDefinition.health - damgage;
         if(_enemyDefinition.health <= 0)
         {
-            _isDead = true;
-            AddScore(_enemyDefinition.score);
-            PopScore();
             Die();
         }
     }
-
     void Die()
     {
+        _isDead = true;
+
         _audioPlayer.PlayOneShot(_deadClip);
         _anim.SetBool("IsDead", _isDead);
 
-        _moveVector = Vector2.zero;
-
-        
+        Stop();
+        AddScore(_enemyDefinition.score);
+        PopScore();
 
         Destroy(gameObject, _deadClip.length);
     }
     void Flip()
     {
         _faceDirection *= -1;
-        wallCheckPoint.localPosition = new Vector3(-wallCheckPoint.localPosition.x, 0f, 0f);
-        _isWallDetected = false;
     }
     void WallCheck()
     {
-        RaycastHit2D hit =  Physics2D.Raycast(wallCheckPoint.position, new Vector3(_faceDirection, 0f, 0f), wallCheckDistance, layer);
-        if(hit.collider != null && hit.collider.gameObject.CompareTag("Pipe"))
-        {
-            _isWallDetected = true;
-        }
+        Vector2 pos = new Vector2(_box.bounds.center.x, _box.bounds.center.y);
+        pos.x = _faceDirection < 0 ? pos.x - _box.size.x / 2 : pos.x + _box.size.x / 2;
+        float offset = 0.075f;
+
+        _isWallDetected = PhysicUtil.ObstacleCheck(pos, _faceDirection < 0, _distanceToObs, _layerToCheck, offset, _debugDraw);
+    }
+    void GroundCheck()
+    {
+        float offset = 0.01f;
+        Vector2 _groundPosition = new Vector2(_box.bounds.center.x, _box.bounds.center.y - _box.size.y / 2);
+
+        _isGrounded = PhysicUtil.GroundCheck(_groundPosition, _distanceBtwPoints, _distanceToGnd, _layerToCheck, offset, _debugDraw);
     }
     void PopScore()
     {
         Vector3 position = new Vector3(transform.position.x, transform.position.y + 0.3f, 0f);
         UIManager.Instance.PopScore(position, _enemyDefinition.score);
     }
-    void OnDrawGizmosSelected()
-    {
-        Gizmos.DrawLine(wallCheckPoint.position, wallCheckPoint.position + new Vector3(wallCheckDistance, 0f, 0f));
-    }
-
     public void AddScore(int score)
     {
         GameManager.Instance.AddScore(score);
